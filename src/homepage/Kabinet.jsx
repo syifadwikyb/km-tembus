2ecd1ae58659bea932506f123c7f0e7ab94483d0
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2025-01-23 15:00:47 +0700

    update

--- src/homepage/Kabinet.jsx
+++ src/homepage/Kabinet.jsx
@@ -10,7 +10,7 @@ export default function Kabinet() {
         <div className="container mx-auto mt-16 pt-0 p-10 lg:p-20 bg-white rounded-lg lg:rounded-xl">            
             <div className="flex flex-col lg:flex-row items-center">
                 <div className="flex justify-center px-4 flex-1 lg:order-2">
-                    <img className="w-1/2 md:w-2/5 lg:w-3/5 max-w-full h-auto" src={kabinet} alt="Bahtera Abhinaya" />
+                    <img className="w-80 md:w-96 lg:w-3/4 max-w-full h-auto" src={kabinet}/>
                 </div>
                 <div className="text-center lg:text-left flex-1 lg:order-1 mr-0 lg:mr-8 mb-6">
                     <p className="text-2xl md:text-3xl lg:text-4xl font-bold mb-3">Bahtera Abhinaya</p>
@@ -25,23 +25,23 @@ export default function Kabinet() {
                 <div className="flex flex-col items-center text-center">
                     <img className="w-32 md:w-48 lg:w-64 h-auto mb-4" src={kapal}/>
                     <p className="font-bold text-lg">Kapal</p>
-                    <p className="text-base md:text-lg lg:text-xl">Perjalanan yang dipenuhi semangat untuk mengarungi rintangan demi meraih tujuan bersama</p>
+                    <p className="text-base md:text-lg lg:text-xl">Perjalanan yang dipenuhi semangat untuk mengarungi rintangan demi meraih tujuan bersama.</p>
                 </div>
                 <div className="flex flex-col items-center text-center">
                     <img className="w-32 md:w-48 lg:w-64 h-auto mb-4" src={sayap}/>
                     <p className="font-bold text-lg">Kapal</p>
-                    <p className="text-base md:text-lg lg:text-xl">Harapan, kemajuan, dan kemampuan untuk terbang lebih tinggi, membawa KM TEMbUS ke level yang lebih baik</p>
+                    <p className="text-base md:text-lg lg:text-xl">Harapan, kemajuan, dan kemampuan untuk terbang lebih tinggi, membawa KM TEMbUS ke level yang lebih baik.</p>
                 </div>
                 <div className="flex flex-col items-center text-center">
                     <img className="w-32 md:w-48 lg:w-64 h-auto mb-4" src={perisai}/>
                     <p className="font-bold text-lg">Kapal</p>
-                    <p className="text-base md:text-lg lg:text-xl">Perlindungan dan rasa aman, memastikan bahwa seluruh anggota KM TEMBUS merasa terlindungi dalam menghadapi segala risiko
+                    <p className="text-base md:text-lg lg:text-xl">Perlindungan dan rasa aman, memastikan bahwa seluruh anggota KM TEMBUS merasa terlindungi dalam menghadapi segala risiko.
                     </p>
                 </div>
                 <div className="flex flex-col items-center text-center">
                     <img className="w-32 md:w-48 lg:w-64 h-auto mb-4" src={tangan}/>
                     <p className="font-bold text-lg">Kapal</p>
-                    <p className="text-base md:text-lg lg:text-xl">Dukungan, kerja sama, dan kebersamaan dalam mewadahi setiap anggota KM TEMBUS untuk saling menguatkan</p>
+                    <p className="text-base md:text-lg lg:text-xl">Dukungan, kerja sama, dan kebersamaan dalam mewadahi setiap anggota KM TEMBUS untuk saling menguatkan.</p>
                 </div>
                 <div className="flex flex-col items-center text-center">
                     <img className="w-32 md:w-48 lg:w-64 h-auto mb-4" src={ombak}/>
